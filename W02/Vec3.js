a29f1acebc9d03ba9269f9f1725efb5d83e6e363
亀井　航太 <170x206x@istc-a-119.epc.center.kobe-u.ac.jp>
2017-04-12 14:47:56 +0900

    Initial commit

---
+++ W02/Vec3.js
@@ -0,0 +1,66 @@
+//Constructor
+Vec3 = function(x,y,z)
+{
+  this.x=x;
+  this.y=y;
+  this.z=z;
+}
+
+Vec3.prototype.min = function()
+{
+var a;
+if(this.x<this.y)
+  {
+    a=this.x;
+  }
+else
+  {
+    a=this.y;
+  }
+if(a>this.z)
+  {
+    a=this.z;
+  }
+  return a;
+}
+
+Vec3.prototype.mid = function()
+{
+var a;
+if(this.x<this.y)
+  {
+    a=this.y;
+  }
+else
+  {
+    a=this.x;
+  }
+if(a>this.z)
+  {
+    a=this.z;
+  }
+}
+
+Vec3.protptype.max = function()
+{
+var a;
+if(this.x>this.y)
+  {
+    a=this.x;
+  }
+else
+  {
+    a=this.y;
+  }
+if(a<this.z)
+  {
+    a=this.z;
+  }
+  return a;
+}
+
+
+
+
+
+
